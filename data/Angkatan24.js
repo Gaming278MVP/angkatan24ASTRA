3563686fea38e5383def3a860c6d23f91ef849e6
HanzProstama <47862061+Gaming278WENAK@users.noreply.github.com>
2019-04-30 14:20:41 +0700

    Update Angkatan24.js

--- data/Angkatan24.js
+++ data/Angkatan24.js
@@ -3,20 +3,20 @@ const fetchUser = require("./fetchUser");
 
 module.exports = class Angkatan24 {
     constructor(NameID, IDCard) {
-        if (!botID) throw new Error("No bot ID was provided. Need Help? discord.gg/c5dMfsF");
-        if (!ownerID) throw new Error("No owner ID was provided. Need Help? discord.gg/c5dMfsF");
-        if (typeof botID !== "string") throw new Error("Bot ID must be a string")
-        if (typeof ownerID !== "string") throw new Error("Owner ID must be a string")
-        if (isNaN(botID)) throw new Error("Invalid Bot ID.");
-        if (isNaN(ownerID)) throw new Error("Invalid Owner ID.");
-        if (botID.length <= 17) throw new Error("Invalid Bot Id.");
-        if (ownerID.length <= 17) throw new Error("Invalid Owner Id.");
+        if (!NameID) throw new Error("No bot ID was provided. Need Help? discord.gg/c5dMfsF");
+        if (!IDCard) throw new Error("No owner ID was provided. Need Help? discord.gg/c5dMfsF");
+        if (typeof NameID !== "string") throw new Error("Bot ID must be a string")
+        if (typeof IDCard !== "string") throw new Error("Owner ID must be a string")
+        if (isNaN(NameID)) throw new Error("Invalid Bot ID.");
+        if (isNaN(IDCard)) throw new Error("Invalid Owner ID.");
+        if (NameID.length <= 17) throw new Error("Invalid Bot Id.");
+        if (IDCard.length <= 17) throw new Error("Invalid Owner Id.");
         
-        request.get(`https://rend-dev.glitch.me/api/bots/${botID}`).then(rend => {
-            if (ownerID !== rend.body.ownerID) throw new Error("Wrong Owner ID.")
+        request.get(`https://angkatan24.glitch.me/data/api/bots/${botID}`).then(a24 => {
+            if (IDCard !== a24.body.NameID) throw new Error("Wrong Owner ID.")
             else {
-                fetchUser(ownerID).then(owner => {
-                    fetchUser(botID).then(botOwn => {
+                fetchUser(IDCard).then(owner => {
+                    fetchUser(NameID).then(botOwn => {
                         console.log(`You logged as ${owner.tag} with bot ${botOwn.tag}`);
                     })
                 })
@@ -24,17 +24,17 @@ module.exports = class Angkatan24 {
         })
         
         this.version = require("../package.json").version;
-        this.baseURL = "https://rend-dev.glitch.me";
-        this.baseAPIURL = this.baseURL + "/api";
+        this.baseURL = "https://angkatan24.glitch.me";
+        this.baseAPIURL = this.baseURL + "/data/api";
         
         this.getBots = async (data = {}) => {
             let {body: bots} = await request.get(this.baseAPIURL + "/botsArray");
-            if (data.ownerID && data.limit) {
-                let botsfilter = bots.filter(bot => bot.ownerID === data.ownerID);
+            if (data.IDCard && data.limit) {
+                let botsfilter = bots.filter(bot => bot.IDCard === data.IDCard);
                 if (data.limit > botsfilter.length) throw Error("limit more than bot data was registered");
                 return botsfilter.splice(0, data.limit);
-            } else if (data.ownerID) {
-                return bots.filter(bot => bot.ownerID === data.ownerID);
+            } else if (data.IDCard) {
+                return bots.filter(bot => bot.IDCard === data.IDCard);
             } else if (data.limit) {
                 if (data.limit > bots.length) throw Error("limit more than bot data was registered");
                 return bots.splice(0, data.limit);
@@ -47,8 +47,8 @@ module.exports = class Angkatan24 {
             if (!id) throw Error("No bot ID was provided");
             const { body: botuser } = await request.get(this.baseAPIURL + `/bots/${id}`);
             if (botuser.error === "not_found") throw Error("Unregistered Bot.")
-            const owner = await fetchUser(botuser.ownerID);
-            const bot = await fetchUser(botuser.botID);
+            const owner = await fetchUser(botuser.IDCard);
+            const bot = await fetchUser(botuser.NameID);
             const body = {
                 owner,
                 bot,
